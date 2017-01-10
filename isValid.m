96053a0609ce30cd61fc5d21c7131d0cb5ecc387
remkruk <kruk.remigiusz@gmail.com>
2017-01-10 20:19:25 +0100

    fix

--- isValid.m
+++ isValid.m
@@ -14,7 +14,7 @@ else
     if ( (iy - fy) == 1 && abs(fx - ix) == 1 && board(fx,fy) == -1)
         valid = 1;
     elseif ( (iy - fy) == 2 && (fx - ix) == 2 && board(fx,fy) == -1 && board(fx-1,fy+1) == 2)
-        board(fx+1,fy+1) = -1;
+        board(fx-1,fy+1) = -1;
         valid = 1; 
     elseif ((iy - fy) == 2 && (fx - ix) == -2 && board(fx,fy) == -1 && board(fx+1,fy+1) == 2 )
         board(fx+1,fy+1) = -1;
